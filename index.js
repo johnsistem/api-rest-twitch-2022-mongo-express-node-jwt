c2f38eb5707e39e541ca5c707db6d8626a16292e
John <johnnyqm5@gmail.com>
2022-06-16 16:39:05 -0600

    mas cambios
    Signed-off-by: John <johnnyqm5@gmail.com>

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/links", linkRouter);
 
 // solo para el ejemplo de login/token
-// app.use(express.static("public"));
+ app.use(express.static("public"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("🔥🔥🔥 http://localhost:" + PORT));
--- utils/tokenManager.js
+++ utils/tokenManager.js
@@ -1,8 +1,11 @@
 import jwt from "jsonwebtoken";
+
+//Genera el token
 export const generateToken = (uid) => {
-    const expiresIn = 60 * 15;
+    const expiresIn = 60 * 15; //tiempo para expirar
 
     try {
+        //se genera el token:id,clave secreta y tiempo de expirar
         const token = jwt.sign({ uid }, process.env.JWT_SECRET, { expiresIn });
         return { token, expiresIn };
     } catch (error) {
@@ -13,10 +16,11 @@ export const generateToken = (uid) => {
 export const generateRefreshToken = (uid, res) => {
     const expiresIn = 60 * 60 * 24 * 30;
     try {
+        //genera el refreshtoken
         const refreshToken = jwt.sign({ uid }, process.env.JWT_REFRESH, {
             expiresIn,
         });
-
+            //guarda el refretoken en la cookies y lo retorna
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
             secure: !(process.env.MODO === "developer"),
